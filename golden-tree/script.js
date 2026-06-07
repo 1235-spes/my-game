c9542fa463cc334b2212fc868ddf256c4f902de0
1235-spes <salhe9849@gmail.com>
2026-06-07 23:51:00 +0300

    Update script.js

--- golden-tree/script.js
+++ golden-tree/script.js
@@ -1,4 +1,5 @@
-const canvas = document.getElementById("gameCanvas");
+alert("script loaded");
+constt canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 // ضبط حجم الكانفاس
