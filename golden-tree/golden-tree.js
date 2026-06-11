088562a7d472d2ed83f9966e1ae0201c71da9a81
1235-spes <salhe9849@gmail.com>
2026-06-11 03:02:54 +0300

    Update golden-tree.js
    ..

--- golden-tree/golden-tree.js
+++ golden-tree/golden-tree.js
@@ -1,1 +1,5 @@
 
+<div class="game-card" onclick="location.href='golden-tree/index.html'">
+    <img class="game-image" src="images/IMG_٢٠٢٦٠٦١٠_٠٠٣٩٢٤.jpg">
+    <div class="game-name">Golden Tree</div>
+</div>
