899f8b367e9290d18b1431dde165ba23163e7206
1235-spes <salhe9849@gmail.com>
2026-06-11 03:19:02 +0300

    golden-tree-css

--- golden-tree/golden-tree.js
+++
@@ -1,5 +0,0 @@
-
-<div class="game-card" onclick="location.href='golden-tree/index.html'">
-    <img class="game-image" src="images/IMG_٢٠٢٦٠٦١٠_٠٠٣٩٢٤.jpg">
-    <div class="game-name">Golden Tree</div>
-</div>
