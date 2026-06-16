704f479c515d055574f9ff7cff22142ab3e26abd
1235-spes <salhe9849@gmail.com>
2026-06-16 15:43:14 +0300

    Update game-core.js

--- game-core.js
+++ game-core.js
@@ -1,1 +1,55 @@
 
+const GameCore = {
+  user: null,
+  balance: 0,
+
+  init(userId) {
+    this.user = userId;
+
+    return firebase.database()
+      .ref("users/" + userId)
+      .get()
+      .then(snap => {
+        const data = snap.val() || { balance: 1000 };
+
+        this.balance = data.balance;
+
+        this.syncUI();
+
+        return this.balance;
+      });
+  },
+
+  syncUI() {
+    const el = document.getElementById("balance");
+    if (el) el.innerText = this.balance;
+  },
+
+  canBet(amount) {
+    return this.balance >= amount;
+  },
+
+  bet(amount) {
+    if (!this.canBet(amount)) {
+      alert("❌ رصيد غير كافي");
+      return false;
+    }
+
+    this.balance -= amount;
+    this.save();
+    this.syncUI();
+    return true;
+  },
+
+  win(amount) {
+    this.balance += amount;
+    this.save();
+    this.syncUI();
+  },
+
+  save() {
+    firebase.database()
+      .ref("users/" + this.user)
+      .update({ balance: this.balance });
+  }
+};
