0b45e22face519fe34174532ad3da63121ae4dff
1235-spes <salhe9849@gmail.com>
2026-06-17 18:15:54 +0300

    Update bonus-frenzy.js
    ..

--- bonus-frenzy/bonus-frenzy.js
+++ bonus-frenzy/bonus-frenzy.js
@@ -1,6 +1,6 @@
 let selectedBet = 300;
 let balance = 0;
-const spinSound = new Audio("../sounds/spin.mp3");
+const spinSound = new Audio("../sounds/صوت دوران البكرات.m4a");
 spinSound.loop = true;
 spinSound.volume = 0.4;
 
