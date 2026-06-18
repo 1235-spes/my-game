cc3a1ac84aaea01ff04de76ff9e51f9b7b8c09b7
1235-spes <salhe9849@gmail.com>
2026-06-18 22:02:51 +0300

    Update bonus-frenzy.js
    ..

--- bonus-frenzy/bonus-frenzy.js
+++ bonus-frenzy/bonus-frenzy.js
@@ -142,30 +142,7 @@ function spin() {
   document.getElementById("balance").innerText = balance;
 setTimeout(() => {
 
-  firebase.database().ref("jackpot/global").transaction(jp => {
-
-    if (!jp) {
-      jp = {
-        spade: 0,
-        club: 0,
-        diamond: 0,
-        heart: 0,
-        jackpotValue: 0
-      };
-    }
-
-    let add = Math.floor(selectedBet * 0.05);
-
-    let keys = ["spade", "club", "diamond", "heart"];
-    let key = keys[Math.floor(Math.random() * keys.length)];
-
-    jp[key] += add;
-    jp.jackpotValue += add;
-
-    return jp;
-  });
-
-}, 0);
+  
   firebase.database().ref("users/" + currentUser).update({ balance });
 
   // 🎰 تشغيل الصوت
