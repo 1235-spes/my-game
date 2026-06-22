b0cfb97c148e6c9edd74e0eb28aa93acd2f77cf7
1235-spes <salhe9849@gmail.com>
2026-06-22 19:44:04 +0300

    Update bonus-frenzy.js
    ..

--- bonus-frenzy/bonus-frenzy.js
+++ bonus-frenzy/bonus-frenzy.js
@@ -498,7 +498,7 @@ function startAutoSpin(times) {
 
     // ⛔ ننتظر وقت أطول من spin الحقيقي
     // (1200 + colIndex*400 + checkWin delay ≈ 2500ms آمن)
-    setTimeout(runNext, 3000);
+    setTimeout(runNext, 3500);
   }
 
   runNext();
