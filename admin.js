7586354c94eefd587f14758e8de4704c196b5528
1235-spes <salhe9849@gmail.com>
2026-06-26 21:37:28 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -112,29 +112,149 @@ function loadSubAdmins(){
       }
 
       html += `
-      <div class="user-card">
+<div class="user-card">
 
-        <div class="user-name">${name}</div>
+<div class="user-name">${name}</div>
 
-        <div class="user-info">
-          💰 الرصيد : ${sa.balance || 0}
-        </div>
+<div class="user-info">
+💰 الرصيد : ${sa.balance || 0}
+</div>
 
-        <div class="user-info">
-          👥 عدد اللاعبين : ${users}
-        </div>
+<div class="user-info">
+👥 عدد اللاعبين : ${users}
+</div>
 
-        <div class="user-info">
-          💵 مجموع أرصدتهم : ${totalBalance}
-        </div>
+<div class="user-info">
+💵 مجموع أرصدتهم : ${totalBalance}
+</div>
 
-      </div>
-      `;
+<input
+type="number"
+class="amount-input"
+id="subAmount-${name}"
+placeholder="اكتب المبلغ">
+
+<div class="actions">
+
+<button
+class="btn add-btn"
+onclick="addSubBalance('${name}')">
+إضافة رصيد
+</button>
+
+<button
+class="btn remove-btn"
+onclick="removeSubBalance('${name}')">
+خصم رصيد
+</button>
+
+</div>
+
+</div>
+`;
 
     });
 
     document.getElementById("subAdminsList").innerHTML = html;
 
   });
+}
+function addSubBalance(name){
+
+const amount = Number(document.getElementById("subAmount-"+name).value || 0);
+
+if(amount<=0){
+alert("أدخل مبلغ صحيح");
+return;
+}
+
+const doktorRef = db.ref("users/doktor");
+const subRef = db.ref("subAdmins/"+name);
+
+doktorRef.get().then(d=>{
+
+const doktor=d.val();
+
+const doktorBalance=Number(doktor.balance||0);
+
+if(doktorBalance<amount){
+
+alert("رصيد الدكتور غير كافي");
+
+return;
+
+}
+
+subRef.get().then(s=>{
+
+const sub=s.val();
+
+const subBalance=Number(sub.balance||0);
+
+doktorRef.update({
+
+balance:doktorBalance-amount
+
+});
+
+subRef.update({
+
+balance:subBalance+amount
+
+});
+
+});
+
+});
+
+}
+function removeSubBalance(name){
+
+const amount = Number(document.getElementById("subAmount-"+name).value || 0);
+
+if(amount<=0){
+alert("أدخل مبلغ صحيح");
+return;
+}
+
+const doktorRef = db.ref("users/doktor");
+const subRef = db.ref("subAdmins/"+name);
+
+subRef.get().then(s=>{
+
+const sub=s.val();
+
+const subBalance=Number(sub.balance||0);
+
+if(subBalance<amount){
+
+alert("رصيد Sub Admin غير كافي");
+
+return;
+
+}
+
+doktorRef.get().then(d=>{
+
+const doktor=d.val();
+
+const doktorBalance=Number(doktor.balance||0);
+
+subRef.update({
+
+balance:subBalance-amount
+
+});
+
+doktorRef.update({
+
+balance:doktorBalance+amount
+
+});
+
+});
+
+});
+
 }
 loadSubAdmins();
