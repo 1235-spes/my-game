e3ec4f354e5ed1efdd82b01495bc69fe38ca6658
1235-spes <salhe9849@gmail.com>
2026-06-27 03:45:36 +0300

    Update bonus-system.js

--- bonus-frenzy/bonus-system.js
+++ bonus-frenzy/bonus-system.js
@@ -1,1 +1,37 @@
 
+// =========================
+// BONUS ENGINE
+// =========================
+
+let bonus = {
+
+    active: false,
+
+    totalSpins: 0,
+
+    spinsLeft: 0,
+
+    totalWin: 0,
+
+    cost: 0
+
+};
+
+// بدء البونص
+function startBonus(spins, cost){
+
+    if(bonus.active) return;
+
+    bonus.active = true;
+
+    bonus.totalSpins = spins;
+
+    bonus.spinsLeft = spins;
+
+    bonus.totalWin = 0;
+
+    bonus.cost = cost;
+
+    console.log("BONUS STARTED");
+
+}
